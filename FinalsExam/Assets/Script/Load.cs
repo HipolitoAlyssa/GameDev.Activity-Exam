d2d493c7e95cd05120a1361c951f8443269066e8
Alyssa Joy Hipolito <92780621+HipolitoAlyssa@users.noreply.github.com>
2021-12-17 22:54:02 -0700

    FinalExam
    Sir kay Angelica Lorraine Bautista ito
    yung akin sir pangalan ay Finals

---
+++ FinalsExam/Assets/Script/Load.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Load : MonoBehaviour
+{
+    public Text FullBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        FullBox.text = PlayerPrefs.GetString("full");
+    }
+    
+    public void clickreset()
+    {
+        PlayerPrefs.DeleteKey("full");
+        SceneManager.LoadScene("1stScene");
+    }
+}
---
+++ FinalsExam/Assets/Script/Play.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Play : MonoBehaviour
+{
+    public InputField textBox;
+
+    public void clickPlay()
+    {
+        PlayerPrefs.SetString("full", textBox.text);
+        SceneManager.LoadScene("2ndScene");
+    }
+}
